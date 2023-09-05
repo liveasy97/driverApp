67d3bc65be4d582c3ca19601aba97c1c83666e9c
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2023-09-05 22:58:34 +0530

    Splash screen design changed that navigates to get transporter data.

--- lib/screens/spashScreenToGetTransporterData.dart
+++ lib/screens/spashScreenToGetTransporterData.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
+
 import 'package:driver_app/constants/color.dart';
 import 'package:driver_app/constants/spaces.dart';
 import 'package:driver_app/controller/transporterIdController.dart';
+import 'package:driver_app/screens/navigationScreen.dart';
+import 'package:flutter/material.dart';
 //import 'package:liveasy/functions/trasnporterApis/runTransporterApiPost.dart';
 import 'package:get/get.dart';
-import 'package:driver_app/screens/navigationScreen.dart';
+import 'package:get_storage/get_storage.dart';
+import '../constants/fontSize.dart';
+import '../constants/fontWeights.dart';
 
 class SplashScreenToGetTransporterData extends StatefulWidget {
   final String mobileNum;
@@ -74,8 +77,9 @@ class _SplashScreenToGetTransporterDataState
         transporterIdController.updateName(name!);
         transporterIdController.updateCompanyName(companyName!);
         print("transporterID is $transporterId");
-        
-        Timer(Duration(milliseconds: 1200), () => Get.off(() => NavigationScreen()));
+
+        Timer(Duration(milliseconds: 1200),
+            () => Get.off(() => NavigationScreen()));
       }
       //Timer(Duration(milliseconds: 1), () => Get.off(() => NavigationScreen()));
     }
@@ -83,40 +87,81 @@ class _SplashScreenToGetTransporterDataState
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: statusBarColor,
+      backgroundColor: darkBlueColor,
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [shadowGrey, white])),
-          padding: EdgeInsets.only(right: space_2, top: space_35),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image(image: AssetImage("assets/images/liveasyTruck.png")),
-              SizedBox(
-                height: space_2,
-              ),
-              Container(
-                child: Column(
-                  children: [
-                    Image(
-                      image: AssetImage("assets/images/logoSplashScreen.png"),
-                      height: space_12,
+          child: Stack(
+        children: [
+          Positioned(
+            left: -175,
+            top: space_1,
+            child: Container(
+              width: width * 5,
+              height: height * 2,
+              color: darkBlueColor,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: width * 0.9,
+                    top: height * 0.45,
+                    child: Text(
+                      'Liveasy',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: size_16,
+                        fontFamily: 'Montserrat',
+                        fontWeight: boldWeight,
+                      ),
                     ),
-                    SizedBox(
-                      height: space_3,
+                  ),
+                  Positioned(
+                    left: width * 0.75,
+                    top: height * 0.455,
+                    child: Image(
+                      image: const AssetImage("assets/images/liveasyLogo.png"),
+                      fit: BoxFit.cover,
+                      height: space_6,
                     ),
-                  ],
-                ),
+                  ),
+                  Positioned(
+                    left: width * 0.95,
+                    top: height * 0.55,
+                    child: Container(
+                      width: width * 0.55,
+                      height: height * 0.4,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage("assets/images/logoRight.png"),
+                        fit: BoxFit.fill,
+                      )),
+                    ),
+                  ),
+                  Positioned(
+                    left: width * 0.4,
+                    top: -10,
+                    child: Transform(
+                      transform: Matrix4.identity()
+                        ..translate(0.0, 0.0)
+                        ..rotateZ(0.02),
+                      child: Container(
+                        width: width * 0.55,
+                        height: height * 0.4,
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                          image: AssetImage("assets/images/logoLeft.png"),
+                          fit: BoxFit.fill,
+                        )),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        ],
+      )),
     );
   }
 }
